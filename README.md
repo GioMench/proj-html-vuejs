abeef4120d4aebbaf50d627e1797dcf7da688a61
GioMench <giorgiaglm@gmail.com>
2024-04-08 19:06:06 +0200

    installe bootstrap set basic structure for nav bar

--- README.md
+++ README.md
@@ -1,6 +1,12 @@
-# Vue 3 + Vite
+# giorno 1
+- studio della struttura 
+- divido ogni sezione in un componente
+# giorno 2 
+- sviluppo i componenti
 
-This template should help get you started developing with Vue 3 in Vite. The template uses Vue 3 `<script setup>` SFCs, check out the [script setup docs](https://v3.vuejs.org/api/sfc-script-setup.html#sfc-script-setup) to learn more.
+# giorno 3 
+- definisco il css
+- pulisco il codice 
 
 ## Recommended IDE Setup
 
---
+++ src/components/HeaderApp.vue
@@ -0,0 +1,61 @@
+<script>
+
+export default{
+    name: 'HeaderApp'
+}
+</script>
+
+<template>
+
+  <section class=" section_header">
+    <!--navbar-->
+    <div class="container-md d-flex justify-content-between p-3">
+        <!--left-side-navbar-->
+        <div class="navbar_left"> 
+            <div>
+                via
+            </div>
+            <div>
+                logo
+            </div>
+        </div>
+        <!--right-side-navbar-->
+        <div class="navbar_right">
+
+            <div>
+                <span>
+                    numero
+                </span>
+                <span>
+                    btn Donate
+                </span>
+            </div>
+
+            <div>
+                list dinamica
+            </div>
+        </div>
+
+
+    </div>
+  </section>
+   
+
+
+   
+</template>
+
+<style>
+@import 'bootstrap/dist/css/bootstrap.css';
+@import 'bootstrap-vue/dist/bootstrap-vue.css';
+@import url('https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&display=swap');
+
+.section_header{
+    height: 180px;
+    background-color: rgba(0, 0, 0, 0.431);
+    z-index: 1;
+
+}
+
+
+</style>
