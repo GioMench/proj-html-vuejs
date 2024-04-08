ae078e7f5e6ee75ed55c3d13080981c379a12937
GioMench <giorgiaglm@gmail.com>
2024-04-08 19:22:50 +0200

    set left side of nav bar

--- src/components/HeaderApp.vue
+++ src/components/HeaderApp.vue
@@ -9,14 +9,17 @@ export default{
 
   <section class=" section_header">
     <!--navbar-->
-    <div class="container-md d-flex justify-content-between p-3">
+    <div class="container-md d-flex justify-content-between p-4 text-light fw-light" style="height: 100%;">
         <!--left-side-navbar-->
-        <div class="navbar_left"> 
+        <div class="navbar_left d-flex flex-column justify-content-between"> 
+            <!--address-academy-->
             <div>
-                via
+                <span style="color: #FFCD18;"><i class="fa-solid fa-location-dot"></i></span>
+                6767 Santa Monica Blvd,Los Angeles,CA 90038
             </div>
+            <!--logo-academy-->
             <div>
-                logo
+                <img src="/public/images/mt-2236-home-logo.png" alt="">
             </div>
         </div>
         <!--right-side-navbar-->
@@ -51,7 +54,7 @@ export default{
 @import url('https://fonts.googleapis.com/css2?family=Alexandria:wght@100..900&display=swap');
 
 .section_header{
-    height: 180px;
+    height: 130px;
     background-color: rgba(0, 0, 0, 0.431);
     z-index: 1;
 
