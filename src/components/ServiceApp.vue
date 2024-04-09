68a6f675bc0eef91a62026e060ff0f05dc3fb7f4
GioMench <giorgiaglm@gmail.com>
2024-04-09 12:30:59 +0200

    set structure ServiceApp

--- src/components/ServiceApp.vue
+++ src/components/ServiceApp.vue
@@ -8,7 +8,37 @@ export default{
 
 <template>
 
- 
+ <section class="me_container container">
+    <h3 class="text-center text-uppercase fw-semibold">
+        our services
+    </h3>
+    <div class="text-center fw-light" style="color: #404040; font-size: 15px;">
+        We make your child happy day after day
+    </div>
+
+    <div class="row">
+        <div class="col-4">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nulla a cupiditate minima sunt, obcaecati quo iure perspiciatis rem magni dolorem officiis, laborum aperiam repudiandae sit! Ducimus eius dolores deleniti natus.
+        </div>
+        <div class="col-8">
+            <div class="row">
+                <div class="col-6">
+                    lorem 
+                </div>
+                <div class="col-6">
+                    lorem 
+                </div>
+                <div class="col-6">
+                    lorem 
+                </div>
+                <div class="col-6">
+                    lorem 
+                </div>
+            </div>
+        </div>
+    </div>
+
+ </section>
 
    
 
