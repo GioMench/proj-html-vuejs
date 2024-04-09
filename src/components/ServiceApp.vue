684b02a37396b61cb541af58512fb35e0d8a50cd
GioMench <giorgiaglm@gmail.com>
2024-04-09 15:08:32 +0200

    add comments in service App

--- src/components/ServiceApp.vue
+++ src/components/ServiceApp.vue
@@ -9,8 +9,7 @@ export default {
                 'Learn to plan, organize, and prioritize',
                 'Regulate emotions',
                 'Learn to pay attention to detail'
-            ],
-
+            ]
             
         }
     }
@@ -45,7 +44,7 @@ export default {
                     Teachers and parents around the world know that chess is not a simple game. It is a great learning
                     tool that will help build your child's executive function skills.
                 </p>
-                <!--list-skills-->
+                <!--list-skills-dinamic-->
                 <ul>
                     <li v-for="skill in skills" class="text_grey fw-light items_skill" style="font-size: 11px;">
                         <span><img class="px-2" src="/public/images/mt-2236-home-icon-bullet.png" alt="chess game"
@@ -53,6 +52,7 @@ export default {
                         {{ skill }}
                     </li>
                 </ul>
+                <!--btn-more-->
                 <button type="button" class="btn px-4 py-2 fw-semibold bg_accent" style="--bs-btn-padding-y: .25rem; --bs-btn-padding-x: .5rem; --bs-btn-font-size: .75rem;">Learn More</button>
 
 
@@ -60,51 +60,63 @@ export default {
             <!--right-section-service-->
             <div class="col-8">
                 <div class="row ">
+                    <!--grid-services-->
                     <div class="col-6 gx-5 ">
                         <div>
+                            <!--icon-service-->
                             <img class="icon_services" src="/public/images/mt-2236-home-icon1.png" alt="icon team" >
                         </div>
+                        <!--title-service-->
                         <div class="fw-semibold text-capitalize" style="font-size: 13px;">
                             various age groups
                         </div>
+                        <!--description-serice-->
                         <p class="text_grey fw-light" style="font-size: 11px;" >
                             We provide chess classes to both children from 5 years old and adult.
                         </p>
                     </div>
                     <div class="col-6 gx-5 ">
                         <div>
+                            <!--icon-service-->
                             <img class="icon_services" src="/public/images/mt-2236-home-icon2.png" alt="icon team" >
                         </div>
+                        <!--title-service-->
                         <div class="fw-semibold text-capitalize" style="font-size: 13px;">
                             top chess variants
                         </div>
+                        <!--description-serice-->
                         <p class="text_grey fw-light" style="font-size: 11px;" >
                             We don't only teach classical chess but also other top variants of this game.
                         </p>
                     </div>
                     <div class="col-6 gx-5 gy-4">
                         <div>
+                            <!--icon-service-->
                             <img class="icon_services" src="/public/images/mt-2236-home-icon3.png" alt="icon team" >
                         </div>
+                        <!--title-service-->
                         <div class="fw-semibold text-capitalize" style="font-size: 13px;">
                             best chess players
                         </div>
+                        <!--description-serice-->
                         <p class="text_grey fw-light" style="font-size: 11px;" >
                             Our alumni have won many prestigious chess tournaments in the country.
                         </p>
                     </div>
                     <div class="col-6 gx-5 gy-4">
                         <div>
+                            <!--icon-service-->
                             <img class="icon_services text-capitalize" src="/public/images/mt-2236-home-icon4.png" alt="icon team" >
                         </div>
+                        <!--title-service-->
                         <div class="fw-semibold" style="font-size: 13px;">
                            best chess openings
                         </div>
+                        <!--description-serice-->
                         <p class="text_grey fw-light" style="font-size: 11px;" >
                             Our students know all the best chess openings and how to use them.
                         </p>
-                    </div>
-                    
+                    </div>  
                 </div>
             </div>
         </div>
